17a14d4c09a810e8cd919462b96fe11795cdc396
salik-a <salika@mef.edu.tr>
2021-10-12 18:44:07 +0300

    loading component and useFetch hook added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import React from 'react';
 import { SafeAreaView, Text } from 'react-native';
-
+import MapView from 'react-native-maps';
+import Loading from './components/Loading';
+import useFetch from './hooks/useFetch';
 
 
 const App = () => {
  
+  const { data, loading, error } = useFetch("https://random-data-api.com/api/users/random_user?size=30");
+  console.log({ data, loading, error })
   return (
-    <SafeAreaView >
-      <Text>hello</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <MapView style={{ flex: 1 }} />
+      {loading && <Loading />}
     </SafeAreaView>
   );
 };
